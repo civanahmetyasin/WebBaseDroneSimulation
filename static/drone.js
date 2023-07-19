d6d91be4c3a0c53f5ba8df4986658b2c610d3663
Tahir Civan <tahir.civan@samsonvt.com>
2023-07-19 18:46:47 +0200

    Update gltf model loading

--- static/drone.js
+++ static/drone.js
@@ -2,7 +2,10 @@
 // npm install node-hid ws express express-ws
 // node server.js
 // http://localhost:8080/
-
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/DRACOLoader.js';
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
 var ws = new WebSocket('ws://localhost:8080');
 
 var scene = new THREE.Scene();
@@ -13,11 +16,29 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshNormalMaterial();
-var drone = new THREE.Mesh(geometry, material);
+// gltf loader with draco decoder
+var loader = new GLTFLoader();
+loader.setDRACOLoader(new DRACOLoader().setDecoderPath('https://www.gstatic.com/draco/v1/decoders/'));
+var drone;
+loader.load(
+    '/models/Drone.glb',
+    function(gltf) {
+      drone = gltf.scene;
+      drone.position.y = 0;
+      drone.position.x = 0;
+      drone.position.z = 0;
+      drone.scale.set(5,5,5);
+      console.log(drone);
+      scene.add(drone);
+    },
+    undefined,
+    function(error) {
+      console.error(error);
+    });
 
-scene.add(drone);
+var light = new THREE.AmbientLight(0xffffff, 5);
+scene.add(light);
+
 
 
 camera.position.z = 5;
@@ -74,9 +95,28 @@ function scale(value, inMin, inMax, outMin, outMax) {
   return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
+var speed = 0;
+var lastPosition = new THREE.Vector3();
+
+function updateInfo() {
+  var speedElement = document.getElementById('speed');
+  var positionElement = document.getElementById('position');
+  
+  // Calculate the speed as the distance travelled since last frame
+  var distance = drone.position.distanceTo(lastPosition);
+  speed = distance / (1/60); // Assuming animate is running at 60 FPS
+  lastPosition.copy(drone.position);
+
+  speedElement.innerText = 'Speed: ' + speed.toFixed(2);
+  positionElement.innerText = 'Position: (' + drone.position.x.toFixed(2) + ', ' 
+                                         + drone.position.y.toFixed(2) + ', ' 
+                                         + drone.position.z.toFixed(2) + ')';
+}
+
 var animate = function() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  updateInfo();
 };
 
 animate();
